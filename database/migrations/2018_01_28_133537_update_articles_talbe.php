e5cbdbae7dff73f90a0a88e8b71e4bcf65d3ecd6
gitxuzan <m13265000805@163.com>
2018-01-29 21:29:46 +0800

    更新表

---
+++ database/migrations/2018_01_28_133537_update_articles_talbe.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateArticlesTalbe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+            $table->longText('content')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
